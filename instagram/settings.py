ca333be8e4b6d531fe0313d7a5d2e66d2063e6d5
bellahOchola <bellahkenya@gmail.com>
2019-12-18 10:28:45 +0300

    create a database and add its configuration to settings file

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lite.apps.LiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'moringa',
+        'PASSWORD': 'bellah@1972',
     }
 }
 
--- instagram/urls.py
+++ instagram/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('lite.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
