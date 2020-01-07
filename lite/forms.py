18b952aa773a47e405f68e8dfea574ed87072d73
bellahOchola <bellahkenya@gmail.com>
2020-01-07 11:36:32 +0300

    add functionality that enables users comment

--- lite/forms.py
+++ lite/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Image,Profile
+from .models import Profile, Caption, Comment
 
 
 class SignUpForm(UserCreationForm):
@@ -9,19 +9,35 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('username', 'email', 'password1', 'password2')
 
-class LoginForm(UserCreationForm):
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
     class Meta:
         model = User
-        fields = ('username',  'password1',)
+        fields = ('username', 'email')
 
-class UploadImage(forms.ModelForm):
-    class Meta:
-        model = Image
-        fields = ('image', 'caption')
 
-class MyProfile(forms.ModelForm):
+class UpdateUserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('profile_pic', 'bio', 'location')
+        fields = ['name', 'location', 'profile_picture', 'bio']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Caption
+        fields = ('image', 'caption')
+
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment...'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
